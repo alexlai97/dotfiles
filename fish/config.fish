71040b10c8b091eb51952d1f06389cb51e093376
Alex Lai <qq731008253@gmail.com>
2019-03-05 22:03:32 -0500

    Regular Commit

--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,4 @@
-set -xg PATH /home/alex/.stack/programs/x86_64-linux/ghc-tinfo6-8.4.3/bin/ /bin /usr/bin /sbin/ /home/alex/bin /home/alex/.scripts /home/alex/.cargo/bin /home/alex/.local/bin /home/alex/.texlive/2018/bin/x86_64-linux/ /opt/bin /usr/local/bin
-
+set -xg PATH /home/alex/.stack/programs/x86_64-linux/ghc-tinfo6-8.4.3/bin/ /bin /usr/bin /sbin/ /home/alex/bin /home/alex/.scripts /home/alex/.cargo/bin /home/alex/.local/bin /home/alex/.texlive/2018/bin/x86_64-linux/ /opt/bin /usr/local/bin /home/alex/.SpaceVim/bin
 set -xg XDG_CONFIG_HOME /home/alex/.config
 set -xg XDG_DATA_HOME $HOME/.local/share
 set -xg MANPATH $HOME/.texlive/2018/texmf-dist/doc/man $HOME/share/man  /usr/local/share/man /usr/share/man /usr/share/gcc-data/x86_64-pc-linux-gnu/7.3.0/man
@@ -9,10 +8,8 @@ set -xg _JAVA_AWT_WM_NONREPARENTING 1
 
 set -g EDITOR vi
 
-#source ~/.aliases
-# source ~/.config/fish/abbr.fish
-
-# if test "$TERM" = "st-256color"
-#    loadcolor
-# end 
-#fzf_key_bindings
+export SSH='x7lai@linux.student.cs.uwaterloo.ca'
+export WINTER_2019='/home/alex/Documents/School/Winter 2019'
+export DOT_FILES='/home/alex/myrepos/dotfiles'
+export SUCKLESS_REPO='/home/alex/myrepos/suckless_stuff'
+export PLAYGROUND='/home/alex/myrepos/Playground'
