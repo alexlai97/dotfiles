b8a5c053548876fadc0657d216de6992046116ab
Alex Lai <qq731008253@gmail.com>
2019-08-18 14:06:06 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,40 +1,41 @@
 #!/bin/bash
+command -v stow > /dev/null 2>&1 || { echo >&2 "stow is not installed"; exit 1; }
+
 set -x
 
-stow -d . -t ~/ alacritty
-stow -d . -t ~/ awesome
-stow -d . -t ~/ bash
-stow -d . -t ~/ compton
-stow -d . -t ~/ conky
-stow -d . -t ~/ dunst
-stow -d . -t ~/ enpitsu
-stow -d . -t ~/ fcitx
-stow -d . -t ~/ fish
-stow -d . -t ~/ ghc
-stow -d . -t ~/ gtk-2.0
-stow -d . -t ~/ gtk-3.0
-stow -d . -t ~/ i3
-stow -d . -t ~/ i3status
-stow -d . -t ~/ install.sh
-stow -d . -t ~/ mpd
-stow -d . -t ~/ mps-youtube
-stow -d . -t ~/ mutt
-stow -d . -t ~/ ncmpcpp
-stow -d . -t ~/ nvim
-stow -d . -t ~/ ranger
-stow -d . -t ~/ redshift
-stow -d . -t ~/ rofi
-stow -d . -t ~/ scripts
-stow -d . -t ~/ sxhkd
-stow -d . -t ~/ sxiv
-stow -d . -t ~/ themes
-stow -d . -t ~/ tint2
-stow -d . -t ~/ tmux
-stow -d . -t ~/ vim
-stow -d . -t ~/ vimb
-stow -d . -t ~/ wpg
-stow -d . -t ~/ zathura
-stow -d . -t ~/ zim
-stow -d . -t ~/ zsh
+stow -v -d . -t ~/ alacritty
+stow -v -d . -t ~/ awesome
+stow -v -d . -t ~/ bash
+stow -v -d . -t ~/ compton
+stow -v -d . -t ~/ conky
+stow -v -d . -t ~/ dunst
+stow -v -d . -t ~/ enpitsu
+stow -v -d . -t ~/ fcitx
+stow -v -d . -t ~/ fish
+stow -v -d . -t ~/ ghc
+stow -v -d . -t ~/ gtk-2.0
+stow -v -d . -t ~/ gtk-3.0
+stow -v -d . -t ~/ i3
+stow -v -d . -t ~/ i3status
+stow -v -d . -t ~/ mpd
+stow -v -d . -t ~/ mps-youtube
+stow -v -d . -t ~/ mutt
+stow -v -d . -t ~/ ncmpcpp
+stow -v -d . -t ~/ nvim
+stow -v -d . -t ~/ ranger
+stow -v -d . -t ~/ redshift
+stow -v -d . -t ~/ rofi
+stow -v -d . -t ~/ scripts
+stow -v -d . -t ~/ sxhkd
+stow -v -d . -t ~/ sxiv
+stow -v -d . -t ~/ themes
+stow -v -d . -t ~/ tint2
+stow -v -d . -t ~/ tmux
+stow -v -d . -t ~/ vim
+stow -v -d . -t ~/ vimb
+stow -v -d . -t ~/ wpg
+stow -v -d . -t ~/ zathura
+stow -v -d . -t ~/ zim
+stow -v -d . -t ~/ zsh
 
-sudo stow -d . -t / portage
+sudo stow -v -d . -t / portage
